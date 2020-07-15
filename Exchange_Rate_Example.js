117965b0b9542f835f805c531452dc699626bab2
wenlianghuang <wen-liang.huang@cern.ch>
2020-07-15 15:06:23 +0800

    20200715 modified file

--- Exchange_Rate_Example.js
+++ Exchange_Rate_Example.js
@@ -26,7 +26,33 @@ getData((body) => {
   $('.rate-content-cash.text-right.print_hide').each((i,ele)=>{
     rate.push($(ele).text())
   })
-  console.log(rate.length)
+
+  /*
+  Remove the value in the array's index '-'
+  */
+  var indexs = []
+  //var testindex = [0,1,2,3,'-',5,6,'-',8,9,'-',11]
+  var indexVal = 0;
+  var i = -1
+
+  /*
+  Check if the '-' is existed and keep going until there is no '-' value
+  */
+  while((rate.indexOf('-',i+1)!=-1)){
+    indexVal = rate.indexOf('-',i+1)
+    indexs.push(indexVal)
+    i = indexVal
+  }
+  
+  //console.log(indexs)
+  
+  var KeepCheckingValue = 0
+  for(var i in indexs){
+    rate.splice(indexs[i]-KeepCheckingValue,1)
+    KeepCheckingValue += 1
+  }
+  console.log(rate)
+
 })
 
 function getData(callback) {
